da464d85adebe502b0d99d515c87658f771fa321
yeguanhuav <yeguanhua@192.168.0.102>
2019-05-09 14:36:33 +0800

    feature argument now is not required

--- R/plot_stacked_bar.R
+++ R/plot_stacked_bar.R
@@ -3,46 +3,47 @@
 #' This is a function for stacked bar plot.
 #'
 #' @param phyloseq A phyloseq object contain otu table, taxonomy table, sample
-#'                 metadata and phylogenetic tree. If you don't have a phyloseq
-#'                 object, you can input OTU table and metadata to draw stacked
-#'                 barplot.
+#'                 metadata and phylogenetic tree. Set it to NULL if using an
+#'                 OTU table and metadata to draw the stacked barplot.
 #' @param feature The feature that shows in x-axis text with different colors.
-#' @param level  A taxonomy level to plot stacked bar.
+#'                Default is NA, x-axis will show in black.
+#' @param level  A taxonomy level to plot stacked bar. Default is NA. Required
+#'               when using phyloseq object.
 #' @param x_size The front size of x-axis text. Default is 8.
 #' @param legend_position Legend position. Default is "top". legend_position
 #'                        should be one of "none", "left", "right", "bottom",
 #'                        "top".
 #' @param legend_size Lengend size. Default is 10.
-#' @param order A vector of arranged SampleID in wanted order. Default is NA.
-#'              If NA, plot_stacked_bar will automatically arrange samples by
+#' @param order A vector of arranged SampleID in wanted order. Default is NULL.
+#'              If NULL, plot_stacked_bar will automatically arrange samples by
 #'              the most abundance taxonomy in decreasing order.
 #' @param otu_table An OTU table. Taxa are colnames, SampleID are rownames
-#'                  (just like phyloseq).
+#'                  (just like phyloseq). Set it to NULL if using phyloseq.
 #' @param metadata A metadata for the OTU table. SampleID are rownames (just
-#'                 like phyloseq).
+#'                 like phyloseq). Set it to NULL if using phyloseq.
 #' @export
 #' @examples
 #' plot_stacked_bar(demo_phyloseq_object, feature = "diagnosis",
 #'                  level = "Order")
 
-plot_stacked_bar <- function(phyloseq = NA, level = NA, feature = NA,
-                             order = NA, x_size = 8, legend_position = "top",
-                             legend_size = 10, otu_table = NA, metadata = NA) {
+plot_stacked_bar <- function(phyloseq = NULL, level = NA, feature = NA,
+                             order = NULL, x_size = 8, legend_position = "top",
+                             legend_size = 10, otu_table = NULL, metadata = NULL) {
   # Detact variables
-  if (is.na(phyloseq)) {
-    if (is.na(otu_table)) {
+  if (is.null(phyloseq)) {
+    if (is.null(otu_table)) {
       # If no phyloseq, require otu_table
       stop("Argument 'phyloseq' and 'otu_table' are both missing,
            please input a phyloseq object or an OTU table and metadata.")
     } else {
-      if (is.na(metadata)) {
+      if (is.null(metadata)) {
         # If have otu_table, require metadata
         stop("Argument 'metadata' is missing, please input a metadata for the
              OTU table.")
       }
     }
   } else {
-    if (!is.na(otu_table)) {
+    if (!is.null(otu_table)) {
       # If have phyloseq, require no otu_table
       stop("Argument 'phyloseq' and 'otu_table' are both detected, please input
            one of them, not both.")
@@ -53,14 +54,12 @@ plot_stacked_bar <- function(phyloseq = NA, level = NA, feature = NA,
     }
   }
   # First construct otu then convert to percentage
-  if (!is.na(phyloseq)) {
+  if (!is.null(phyloseq)) {
     otu <- construct_otu_table(phyloseq, level)
   } else {
     otu <- otu_table
   }
-  otu_percent <- otu %>%
-    convert_to_percentage(row_sum = TRUE) %>%
-    as.data.frame()
+  otu_percent <- otu %>% convert_to_percentage(row_sum = TRUE)
   # Construct table for stacked bar plot
   plot_tab <- otu_percent %>%
     # First re-order taxonomy by total counts
@@ -72,13 +71,13 @@ plot_stacked_bar <- function(phyloseq = NA, level = NA, feature = NA,
   # Prepare levels for taxonomy levels
   levels_level <- colnames(plot_tab)[2:ncol(plot_tab)]
   # Extract metadata
-  if (is.na(otu_table)) {
-    sample_feature <- extract_metadata_phyloseq(phyloseq, feature)
+  if (is.null(otu_table)) {
+    sample_feature <- extract_metadata_phyloseq(phyloseq)
   } else {
     sample_feature <- metadata %>% rownames_to_column("SampleID")
   }
   # Prepare levels for SampleID
-  if (all(is.na(order))) {
+  if (is.null(order)) {
     levels_SampleID <- plot_tab$SampleID
   } else if (length(order) != length(plot_tab$SampleID)) {
     stop("The length of order and SampleID does not match.")
@@ -95,13 +94,15 @@ plot_stacked_bar <- function(phyloseq = NA, level = NA, feature = NA,
   plot_tab$SampleID <- factor(plot_tab$SampleID, levels = levels_SampleID)
   # Prepare levels for feature (colors in x-axis)
   if (!is.na(feature)) {
-    if (all(is.na(order))) {
-      levels_feature <- plot_tab[,ncol(plot_tab)] %>% as.factor()
+    if (is.null(order)) {
+      levels_feature <- plot_tab[[feature]] %>% as.factor()
     } else {
-      levels_feature <- as.data.frame(order) %>%
+      levels_feature <- as.data.frame(order)
+      colnames(levels_feature) <- "Sort"
+      levels_feature <- levels_feature %>%
         # Arrange feature order by 'order' variable
-        left_join(plot_tab[,c(1, ncol(plot_tab))],
-                  by = c("order" = "SampleID")) %>%
+        left_join(select(plot_tab, SampleID, !!feature),
+                  by = c("Sort" = "SampleID")) %>%
         # Drop levels
         .[[2]] %>% as.character() %>% as.factor()
     }
