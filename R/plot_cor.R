3053a14e20bbda180794c7eb111654dcf9ea0d9b
yeguanhuav <yeguanhua@192.168.0.102>
2019-05-13 14:21:47 +0800

    add plot_cor function

---
+++ R/plot_cor.R
@@ -0,0 +1,63 @@
+#' plot_cor
+#'
+#' plot_cor can plot correlation using a input table.
+#'
+#' @param cor_tab A data frame that contains at least two columns to calculate
+#' correlation.
+#' @param x Colname of cor_tab. x and y must have the same length.
+#' @param y Colname of cor_tab. x and y must have the same length.
+#' @param method A character string indicating which correlation coefficient is
+#' to be used. One of "pearson", "kendall", or "spearman", can be abbreviated.
+#' Default is "pearson".
+#' @export
+
+plot_cor <- function(cor_tab, x, y, method = "pearson") {
+  # Notice: Colnames of the input table can only be letters or numbers.
+  if (any(str_detect(c(x, y), '\\W'))) {
+    stop("Colnames of the input columns can only contain letters or numbers,
+         or it can't be recognized when plotting.")
+  }
+  # Calculate correlation
+  cor_res <- cor.test(cor_tab[[x]], cor_tab[[y]], method = method)
+  if (cor_res$p.value < 2.2e-16) {
+    ggplot(data = cor_tab, aes_string(x = x, y = y)) +
+      geom_point() +
+      geom_smooth(method = lm) +
+      annotate(geom = 'text',
+               x = mean(cor_tab[[x]]),
+               y = max(cor_tab[[y]]) * 1.1,
+               label = paste0('rho = ', round(cor_res$estimate, 3))) +
+      annotate(geom = 'text',
+               x = mean(cor_tab[[x]]),
+               y = max(cor_tab[[y]]) * 1.05,
+               label = paste0('p-value < 2.2e-16')) +
+      labs(x = as.character(x), y = as.character(y)) +
+      theme_bw() +
+      theme(panel.grid = element_blank(),
+            axis.text.y = element_text(size = 12),
+            axis.title = element_text(size = 14),
+            axis.text.x = element_text(size = 12),
+            legend.text = element_text(size = 12),
+            strip.text.x = element_text(size = 14))
+  } else {
+    ggplot(data = cor_tab, aes_string(x = x, y = y)) +
+      geom_point() +
+      geom_smooth(method = lm) +
+      annotate(geom = 'text',
+               x = mean(cor_tab[[x]]),
+               y = max(cor_tab[[y]]) * 1.1,
+               label = paste0('rho = ', round(cor_res$estimate, 3))) +
+      annotate(geom = 'text',
+               x = mean(cor_tab[[x]]),
+               y = max(cor_tab[[y]]) * 1.05,
+               label = paste0('p-value = ', round(cor_res$p.value, 3))) +
+      labs(x = as.character(x), y = as.character(y)) +
+      theme_bw() +
+      theme(panel.grid = element_blank(),
+            axis.text.y = element_text(size = 12),
+            axis.title = element_text(size = 14),
+            axis.text.x = element_text(size = 12),
+            legend.text = element_text(size = 12),
+            strip.text.x = element_text(size = 14))
+  }
+}
