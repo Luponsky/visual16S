d9a39cc5a86018384734754273b78508f70666cf
yeguanhuav <yeguanhua@192.168.0.102>
2019-05-27 23:49:07 +0800

    make the plot cleaner and better looking

--- R/plot_stacked_bar.R
+++ R/plot_stacked_bar.R
@@ -5,38 +5,51 @@
 #' @param phyloseq A phyloseq object contain otu table, taxonomy table, sample
 #' metadata and phylogenetic tree. Set it to NULL if using an OTU table and
 #' metadata to draw the stacked barplot.
+#'
 #' @param feature The feature that shows in x-axis text with different colors.
 #' Default is NA, x-axis will show in black.
+#'
 #' @param level  A taxonomy level to plot stacked bar. Default is NA. Required
 #' when using phyloseq object.
-#' @param x_size The front size of x-axis text. Default is 8.
+#'
 #' @param legend_position Legend position. Default is "top". legend_position
 #' should be one of "none", "left", "right", "bottom", "top".
-#' @param legend_size Lengend size. Default is 10.
+#'
 #' @param order A vector of arranged SampleID in wanted order. Default is NULL.
 #' If NULL, plot_stacked_bar will automatically arrange samples by the most
 #' abundance taxonomy in decreasing order.
+#'
 #' @param otu_table An OTU table. Taxa are colnames, SampleID are rownames
 #' (just like phyloseq). Set it to NULL if using phyloseq.
+#'
 #' @param metadata A metadata for the OTU table. SampleID are rownames (just
 #' like phyloseq). Set it to NULL if using phyloseq.
+#'
 #' @param relative_abundance Turn plot into relative abundance or not. Default
 #' is FALSE.
+#'
 #' @export
+#'
 #' @examples
 #' plot_stacked_bar(demo_phyloseq_object, level = "Order")
 
-plot_stacked_bar <- function(phyloseq = NULL, level = NA, feature = NA,
-                             otu_table = NULL, metadata = NULL, order = NULL,
-                             relative_abundance = FALSE, x_size = 8,
-                             legend_position = "top", legend_size = 10) {
+plot_stacked_bar <- function (
+  phyloseq = NULL,
+  level = NA,
+  feature = NA,
+  otu_table = NULL,
+  metadata = NULL,
+  order = NULL,
+  relative_abundance = FALSE,
+  legend_position = "top"
+) {
   # Detact variables
   if (is.null(phyloseq)) {
     if (is.null(otu_table)) {
       # If no phyloseq, require otu_table
       stop(paste0("Argument 'phyloseq' and 'otu_table' are both missing, ",
-                  "please input a phyloseq object or an OTU table and",
-                  " metadata."))
+                  "please input a phyloseq object or an OTU table with ",
+                  "metadata."))
     } else {
       if (is.null(metadata)) {
         # If have otu_table, require metadata
@@ -71,6 +84,7 @@ plot_stacked_bar <- function(phyloseq = NULL, level = NA, feature = NA,
       .[order(.[,1], decreasing = TRUE),] %>%
       # Turn SampleID to a column
       rownames_to_column(var = "SampleID")
+    y_label <- "Relative Abundance"
   } else {
     plot_tab <- otu %>%
       # First re-order taxonomy by total counts
@@ -79,6 +93,7 @@ plot_stacked_bar <- function(phyloseq = NULL, level = NA, feature = NA,
       .[order(rowSums(.), decreasing = TRUE),] %>%
       # Turn SampleID to a column
       rownames_to_column(var = "SampleID")
+    y_label <- "Raw Count"
   }
   # Prepare levels for taxonomy levels
   levels_level <- colnames(plot_tab)[2:ncol(plot_tab)]
@@ -151,15 +166,18 @@ plot_stacked_bar <- function(phyloseq = NULL, level = NA, feature = NA,
   # Bar plot
   ggplot(plot_tab, aes(x = SampleID, y = abundance)) +
     scale_fill_manual(values = distinctive_colors) +
-    geom_bar(mapping = aes(fill = level), stat = "identity") +
-    theme_bw() +
-    theme(panel.grid = element_blank(),
-          axis.text.y = element_text(size = 12),
-          axis.title = element_text(size = 14),
-          strip.text.x = element_text(size = 14),
-          axis.text.x = element_text(size = x_size, angle = 90,
-                                     # 'color' can only work with factor
-                                     color = levels_feature),
-          legend.text = element_text(size = legend_size),
-          legend.position = legend_position)
+    geom_bar(mapping = aes(fill = level),
+             #width = .1,
+             stat = "identity") +
+    theme_minimal() +
+    theme(
+      #panel.grid = element_blank(), # Remove grid line under the plot
+      panel.border = element_blank(), # Remove border
+      axis.text.x = element_text(angle = 90,
+                                 # 'color' can only work with factor
+                                 color = levels_feature),
+      legend.position = legend_position,
+      legend.title = element_blank()
+    ) +
+    ylab(y_label)
 }
